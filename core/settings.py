a3be0649b0797d6a1fd9f073c81244f381f271f6
Kumar-laxmi <laxmikantk2002@gmail.com>
2022-07-01 10:23:41 +0530

    API usage

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
